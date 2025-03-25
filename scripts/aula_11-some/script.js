2c74e86cf63686fa371307143ddeb5c00adf8e3c
Pedro Carvalho <pedro.juliogc02@gmail.com>
2025-03-24 21:41:04 -0300

    Lesson 11: some

---
+++ scripts/aula_11-some/script.js
@@ -0,0 +1,11 @@
+/*
+    Método some()
+    - Testa se ao menos um dos elementos no array
+    passa na condição e retorna um valor true ou false.
+*/
+
+// Exemplo de array de idades
+const ages = [15, 30, 39, 29];
+
+const result = ages.some((age) => age < 18);
+console.log(result);
